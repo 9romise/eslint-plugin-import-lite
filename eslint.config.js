2fd2028e87803414c105c68457aff8ae48aca9b4
Vida Xie <vida_2020@163.com>
2025-06-14 02:56:08 +0800

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from '@vida0905/eslint-config'
 
 export default defineConfig({
   pnpm: true,
+}, {
+  files: ['src/rules/**/*.ts'],
+  rules: {
+    'no-console': 'off',
+  },
 })
