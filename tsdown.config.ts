60ebcdf85383f6d5d34222e8b41212156bf1456b
Vida Xie <vida_2020@163.com>
2025-06-15 21:27:40 +0800

    build: only bundle dts for index file

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -6,24 +6,30 @@ const rulesEntry = globSync(`src/rules/**/*.ts`, {
   ignore: ['**/*.test.ts', 'src/rules/index.ts'],
 })
 
-export default defineConfig({
-  clean: true,
-  dts: true,
-  alias: {
-    '~': resolve('src'),
-  },
-  entry: {
-    index: 'src/index.ts',
-    ...Object.fromEntries(rulesEntry.map((file) => [`rules/${basename(dirname(file))}`, file])),
-  },
-  outputOptions: {
-    chunkFileNames(info) {
-      if (info.moduleIds.length === 1 && info.moduleIds[0].includes('rules')) {
-        return 'rules/[name].js'
-      }
-      return '[name].js'
+export default defineConfig([
+  {
+    clean: true,
+    dts: false,
+    alias: {
+      '~': resolve('src'),
     },
+    entry: {
+      index: 'src/index.ts',
+      ...Object.fromEntries(rulesEntry.map((file) => [`rules/${basename(dirname(file))}`, file])),
+    },
+    outputOptions: {
+      chunkFileNames(info) {
+        if (info.moduleIds.length === 1 && info.moduleIds[0].includes('rules')) {
+          return 'rules/[name].js'
+        }
+        return '[name].js'
+      },
+    },
+    shims: true,
+    format: 'esm',
   },
-  shims: true,
-  format: 'esm',
-})
+  {
+    entry: ['src/index.ts'],
+    dts: true,
+  },
+])
