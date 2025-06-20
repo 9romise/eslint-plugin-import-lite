e840171ae7843a99d0ca60cfee8510dc46e79cfe
Vida Xie <vida_2020@163.com>
2025-06-20 11:43:42 +0800

    chore: update glob patterns

--- scripts/update.ts
+++ scripts/update.ts
@@ -1,6 +1,6 @@
 import type { JSONSchema4 } from '@typescript-eslint/utils/json-schema'
 import { writeFileSync } from 'node:fs'
-import { resolve } from 'node:path'
+import { basename, resolve } from 'node:path'
 import { camelCase, pascalCase } from 'change-case'
 import { compile } from 'json-schema-to-typescript-lite'
 import { globSync } from 'tinyglobby'
@@ -8,10 +8,10 @@ import { globSync } from 'tinyglobby'
 const GEN_HEADER = '/* GENERATED, DO NOT EDIT DIRECTLY */'
 const RULE_DIR = resolve('src/rules')
 
-const rules = globSync('**/*.ts', {
-  cwd: 'src/rules',
-  ignore: ['**/*.test.ts', 'index.ts', '**/*.d.ts'],
-}).map((i) => i.split('/')[0])
+const rules = globSync('./src/rules/*', {
+  onlyDirectories: true,
+  absolute: true,
+}).map((i) => basename(i))
 
 async function writeRulesIndex() {
   const index = [
