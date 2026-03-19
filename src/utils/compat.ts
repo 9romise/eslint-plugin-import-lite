52e4cc293b526b75469ea4029fa39e5c6c39ade5
Vida Xie <vida_2020@163.com>
2026-03-19 09:08:01 +0800

    feat: support ESLint 10

--- src/utils/compat.ts
+++ src/utils/compat.ts
@@ -1,7 +1,7 @@
 import type { RuleContext } from '~/types'
 
 export function sourceType<MessageIds extends string, Options extends readonly unknown[]>(context: RuleContext<MessageIds, Options>) {
-  if ('sourceType' in context.parserOptions) {
+  if ('parserOptions' in context && 'sourceType' in context.parserOptions) {
     return context.parserOptions.sourceType
   }
   if ('languageOptions' in context && context.languageOptions) {
